dccf4de414e76b1774a1be727626e8bc56f8b9ab
kakuza73 <stas-e93@inbox.ru>
2025-02-08 01:29:42 +0600

    Check

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -14,11 +13,14 @@ export default defineConfig({
       },
       cors: false,
       '/socket.io': {
-        // target: 'ws://localhost:5001',
         target: 'ws://localhost:5001',
         ws: true,
         rewriteWsOrigin: true,
       },
     },
+  },
+  preview: {
+    port: 5002,
+    allowedHosts: ['chat-project-wohn.onrender.com'],
   },
 });
