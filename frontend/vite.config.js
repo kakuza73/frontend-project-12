ad0195ac5a14b6601fc41f2eb869f07e70c0806a
kakuza73 <stas-e93@inbox.ru>
2025-02-08 14:12:13 +0600

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 5002,  // Используем переменную окружения PORT
+    port: process.env.PORT || 5002,
     proxy: process.env.NODE_ENV === 'development' ? {
       '/api': {
         target: 'http://localhost:5001',
@@ -15,7 +15,7 @@ export default defineConfig({
         target: 'ws://localhost:5001',
         ws: true,
       },
-    } : {},  // В продакшне убираем proxy
-    cors: true, // Разрешаем CORS
+    } : {},
+    cors: true,
   },
 });
